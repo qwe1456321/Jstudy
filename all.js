d88d9a0594ea758615f4a8d7ed3c1db2d63ec8d4
qwe1456321 <asdqwe1456321@gmail.com>
2025-04-24 10:10:01 +0800

    JS小項目

---
+++ all.js
@@ -0,0 +1,9 @@
+// 如果達500 以上 成為VIP
+let Aperson = 400;
+let isVip = true;
+console.log(Aperson>=500 && isVip==true);
+
+let apple = true;
+let rice = false;
+let noodle = false;
+console.log(apple == true || rice == true && noodle == true);
